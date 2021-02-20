8e44778356c732586651f2665b507df2af24f2a8
Jhimmy Astoraque Duran <jadsa.corp2@gmail.com>
2021-02-20 12:07:13 -0400

    Student Lab01 done

---
+++ CourseWholeProjects/Lab01_Student/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab01_Student
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> studentsMathCourse = new List<Student>();
+
+            Student student1 = new Student("Jeremias", 5.0f);
+            Student student2 = new Student("Matias", 5.0f);
+            Student student3 = new Student("Helen", 3.0f);
+            Student student4 = new Student("Girlnalda", 5.0f, 1.0f);
+            Student student5 = new Student("Toby", 5.0f, 3.0f);
+
+            studentsMathCourse.Add(student1);
+            studentsMathCourse.Add(student2);
+            studentsMathCourse.Add(student3);
+            studentsMathCourse.Add(student4);
+            studentsMathCourse.Add(student5);
+
+
+            student1.UpdateGPA(3.3f);
+            student1.UpdateGPA(5.3f);
+            student1.UpdateGPA(4.3f);
+            student1.UpdateGPA(2.3f);
+            student1.UpdateGPA(4.3f);
+            
+            // iterate the students
+            foreach (var student in studentsMathCourse)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ CourseWholeProjects/Lab01_Student/Student.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab01_Student
+{
+    class Student
+    {
+        // static var
+        private static uint idCounter = 2021000000; // stars from 2021 year
+
+        // instance variables, member variables, attributes, fields
+        private string name;
+        private uint id;
+        private float gpa;
+        readonly float gpaBased; // gpa can be based on 4.0, 5.0 most common, 6.0
+        private List<float> gradesHistory;
+
+        // props
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public uint ID
+        {
+            get { return id; }
+        }
+
+        public float GPA
+        {
+            get { return gpa; }
+        }
+
+        // constructors
+        public Student(string name, float gpaBased)
+        {
+            this.name = name;
+            this.id = ++idCounter;
+            this.gpa = 0.0f;
+            this.gpaBased = gpaBased;
+            gradesHistory = new List<float>();
+        }
+
+        public Student(string name, float gpaBased , float initialGrade) : this(name, gpaBased)
+        {
+            UpdateGPA(initialGrade);            
+        }
+
+       
+        // public methods
+        public void UpdateGPA(float grade)
+        {
+            if (addGradeToHistory(grade))
+            {
+                gpa = calculateGPA();
+            }
+            else
+            {
+                Console.WriteLine("Could not update the GPA, it keeps being the same");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Student name: {name}, id: {id}, gpa: {gpa}";
+        }
+
+        // helper methods
+        private float calculateGPA()
+        {
+            int size = gradesHistory.Count;
+            float gradesSum = 0;
+            foreach (var grade in gradesHistory)
+            {
+                gradesSum += grade;
+            }
+            return (float)Math.Round((gradesSum / size), 1);
+        }
+
+        private bool addGradeToHistory(float grade)
+        {
+            if (grade >= 0.0 && grade <= gpaBased)
+            {
+                gradesHistory.Add(grade);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid grade!. GPA base is: " + gpaBased + " for this student id: " + id);
+                return false;
+            }
+        }
+
+    } // eof class
+} // eof namespace
+
